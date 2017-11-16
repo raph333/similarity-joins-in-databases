5a1d2960606fbad255903881cf1754efe058762c
Philipp <philipp.grafendorfer@blumatix-consulting.at>
2017-11-16 11:15:57 +0100

    major update, runtime fix, verify whole pairs

--- make_test_sample.py
+++ make_test_sample.py
@@ -2,6 +2,6 @@ from random import *
 
 eingabe = input()
 
-for x in range (1,int(eingabe)):
-	y=randint(1,20)
-	print([int(i) for i in str(randint(1, 10**y))])
+for x in range(1, int(eingabe)):
+    y = randint(1, 20)
+    print([int(i) for i in str(randint(1, 10**y))])
